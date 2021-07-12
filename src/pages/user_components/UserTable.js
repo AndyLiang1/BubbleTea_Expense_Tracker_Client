b6d7aeaf6654da8284a94ee56d8397f918d0bbd8
AndyLiang1 <andyliang1000@gmail.com>
2021-07-11 20:18:33 -0700

    Fixed url

--- src/pages/user_components/UserTable.js
+++ src/pages/user_components/UserTable.js
@@ -110,7 +110,7 @@ function UserTable({
    */
   const fillInLabelsWithPrevValues = () => {
     const userId = authState.id;
-    fetch(`http://localhost:3001/user/purchase/${userId}/${purchaseIdEdit}`, {
+    fetch(`https://bubbletea-expense-tracker.herokuapp.com/${userId}/${purchaseIdEdit}`, {
       headers: {
         accessToken: localStorage.getItem("accessToken"),
       },
